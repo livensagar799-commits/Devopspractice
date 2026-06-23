efa6c8604aaf988919da9ae9fc3133bf3c86b015
sagar <livensagar799@gmail.com>
2026-06-23 19:41:01 +0530

    I added new if file

--- Installation.sh
+++ Installation.sh
@@ -8,12 +8,13 @@ then
     exit 1
 fi
 
-dnf install mysql -y
+dnf list installed mysql 
 
 if [ $? -ne 0 ]
-then 
-    echo "Mysql is intalled nothing to do"
+then
+    echo "Mysql is not intalled going to install" 
+    dnf install mysql -y
     exit 1
 else 
-    echo "Mysql is not intalled going to install"
+     echo "Mysql is intalled nothing to do"
 fi

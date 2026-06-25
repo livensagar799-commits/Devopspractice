a3eeda33e068c7da29d20abc45e5468421374c6d
sagar <livensagar799@gmail.com>
2026-06-25 17:03:05 +0530

    I added new if file

--- for_loop.sh
+++ for_loop.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 USERID=$(id -u)
+LOG_PATH=/var/log/rpminstallation
+Scritpname=$(echo "$0" | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_PATH/$Scritpname-$TIMESTAMP.log"
+mkdir -p /var/log/rpminstallation
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ] 
     then
-        echo "Execute the script with root user"
+        echo "Execute the script with root user" | tee -a $LOG_FILE
     fi
     }
 
-# USAGE(){
-#     if [ $# -e 0]
-#     then
-#         echo "USAGE :: sh $0 packagename1 packagename2"
-#     fi
-#     }
+
 
 CHECK_ROOT
 if [ $# -eq 0 ]
      then
-         echo "USAGE :: sh $0 packagename1 packagename2"
+         echo "USAGE :: sh $0 packagename1 packagename2" | tee -a $LOG_FILE
          exit 1
       fi
 for package in $@
 do
-  dnf list installed $package 
+  dnf list installed $package &>>$LOG_FILE
   if [ $? -ne 0 ]
   then
-    echo "$package is not installed going to intall"
-    dnf install $package -y
+    echo "$package is not installed going to intall" | tee -a $LOG_FILE
+    dnf install $package -y &>>$LOG_FILE
     exit 1
   else 
-    echo "$package is already installed ...nothing to do"
+    echo "$package is already installed ...nothing to do" | tee -a $LOG_FILE
   fi
 done

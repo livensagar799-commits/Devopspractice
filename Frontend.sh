e97ff8d965acb802f69d48a20c6f0bb49924f886
sagar <livensagar799@gmail.com>
2026-07-03 14:31:47 +0530

    I added new if file

--- Frontend.sh
+++ Frontend.sh
@@ -45,7 +45,7 @@ curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expe
 VALIDATE $? "Source code"
 
 cd /usr/share/nginx/html &>>$LOG_FILE
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$LOG_FILE
 
 cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOG_FILE
 VALIDATE $? "Copied expense conf"

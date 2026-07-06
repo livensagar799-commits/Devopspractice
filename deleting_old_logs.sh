117d1e9d6e9fbfdd381353d6b9cdf33c6649f05b
sagar <livensagar799@gmail.com>
2026-07-06 23:01:17 +0530

    I added new if file

--- deleting_old_logs.sh
+++ deleting_old_logs.sh
@@ -2,9 +2,17 @@
 
 SOURCE_DIR="/home/ec2-user/logs"
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
 if [ -d $SOURCE_DIR ]
 then 
-    echo "$SOURCE_DIR Exists"
+    echo -e "$SOURCE_DIR $G Exists $N"
 else
-    echo "$SOURCE_DIR does not Exists"
+    echo "$SOURCE_DIR $R does not Exists $N"
 fi
+
+Log_Files=$(find $SOURCE_DIR -name "*.log" -mtime -14 )
+echo "Files : $Log_Files"

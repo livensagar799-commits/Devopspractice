4be0a226d19ccc97695b6462f7cd849210cddedb
sagar <livensagar799@gmail.com>
2026-07-07 13:00:52 +0530

    I added new if file

---
+++ backup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+
+
+
+USAGE (){
+    echo "USAGE : sh 19-backup.sh <source> <destination> <days(optional)>"
+    ecit 1
+}
+
+if [ $# -lt 2 ]
+then 
+    USAGE
+    exit 1
+fi
+
+if [ -d $SOURCE_DIR ]
+then
+    echo "$SOURCE_DIR does not exist"
+    exit 1
+fi
+
+if [ -d $DEST_DIR ]
+then
+    echo "$DEST_DIR does not exist"
+    exit 1
+fi
+
+
+
+
